92b8df44c12b469269410e3983b519ca04a722fc
tiagoferreira <si.tiagoferreira@gmail.com>
2016-07-12 00:38:40 -0300

    connecting with mongodb

--- routes/app.js
+++ routes/app.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-router.get('/:userTitle?', function(req, res, next) {
+router.get('/', function(req, res, next) {
     res.render('node', {title : req.params.userTitle ? req.params.userTitle : ''});
 });
 
 router.post('/', function(req, res, next){
-   var title = req.body.title;
-   res.redirect('/'+title);
+   var email = req.body.email;
+   var user = new User({
+     firstName: 'Tiago',
+     lastName: 'Ferreira',
+     password: 'biruta',
+     email: email,
+   })
+   user.save();
+   res.redirect('/');
 })
 
 module.exports = router;
